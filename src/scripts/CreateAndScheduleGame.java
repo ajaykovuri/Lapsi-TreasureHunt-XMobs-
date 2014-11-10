2b8faa3b6c3ca177dd68c1534ccc914f9297b30c
ajaykovuri <akajaykovuri@gmail.com>
2014-11-10 20:32:47 +0530

    gitig

--- src/scripts/CreateAndScheduleGame.java
+++ src/scripts/CreateAndScheduleGame.java
@@ -21,7 +21,7 @@ import org.openqa.selenium.interactions.internal.TouchAction;
 import org.openqa.selenium.interactions.touch.TouchActions;
 import org.testng.annotations.Test;
 import testUtils.Helper;
-//ajay after gitignore
+//ajay after gitignore 
 public class CreateAndScheduleGame extends Helper {
 	  DateFormat dateFormat = new SimpleDateFormat("yymmddHHmmss");
 	  Date date = new Date();
